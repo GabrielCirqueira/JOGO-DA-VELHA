13c179236527984c0c63042770c2a8b1c9c050a5
OLD-gabriel <gabrielcirqueira711@gmail.com>
2024-04-27 19:34:34 -0300

    VERIFICAÇÃO SE ALGUEM GANHOU O JOGO

--- jogo_velha.py
+++ jogo_velha.py
@@ -9,21 +9,20 @@ dados = [
 jogada_X = True
 
 def tabela(dados,jogada_X):
+    
+    if(jogada_X == True):
+        print("\nSua Vez Jogador X\n")
+    else:
+        print("\n Sua Vez Jogador O\n")
+        
     print(f" {dados[0][0]} | {dados[0][1]} | {dados[0][2]} ")
     print("-----------")
     print(f" {dados[1][0]} | {dados[1][1]} | {dados[1][2]} ")
     print("-----------")
     print(f" {dados[2][0]} | {dados[2][1]} | {dados[2][2]} ")
     
-    if(jogada_X == True):
-        print("\n\n Sua Vez X\n")
-        local = int(input("DIGITE AONDE DESEJA JOGAR: "))
-        return local
-    
-    else:
-        print("\n\n Sua Vez O\n")
-        local = int(input("DIGITE AONDE DESEJA JOGAR: "))
-        return local
+    local = int(input("DIGITE AONDE DESEJA JOGAR: "))
+    return local
     
 
 def Alterar_jogada():
@@ -34,81 +33,40 @@ def Alterar_jogada():
 while True:
     jogada = tabela(dados,jogada_X)
     
-    if(jogada == 1):
-        if(jogada_X == True):
-            dados[0][0] = "X"
-            Alterar_jogada()
-        else:
-            dados[0][0] = "O"
-            Alterar_jogada()
-            
-    elif jogada == 2:
-        if jogada_X:
-            dados[0][1] = "X"
-            Alterar_jogada()
-        else:
-            dados[0][1] = "O"
-            Alterar_jogada()
-            
-    elif jogada == 3:
-        if jogada_X:
-            dados[0][2] = "X"
-            Alterar_jogada()
-        else:
-            dados[0][2] = "O"
-            Alterar_jogada()
+    linha = (jogada - 1) // 3
+    coluna = (jogada - 1) % 3
 
-    elif jogada == 4:
-        if jogada_X:
-            dados[1][0] = "X"
-            Alterar_jogada()
-        else:
-            dados[1][0] = "O"
-            Alterar_jogada()
-
-    elif jogada == 5:
-        if jogada_X:
-            dados[1][1] = "X"
-            Alterar_jogada()
-        else:
-            dados[1][1] = "O"
-            Alterar_jogada()
-
-    elif jogada == 6:
-        if jogada_X:
-            dados[1][2] = "X"
-            Alterar_jogada()
-        else:
-            dados[1][2] = "O"
-            Alterar_jogada()
-
-    elif jogada == 7:
-        if jogada_X:
-            dados[2][0] = "X"
-            Alterar_jogada()
-        else:
-            dados[2][0] = "O"
-            Alterar_jogada()
-
-    elif jogada == 8:
-        if jogada_X:
-            dados[2][1] = "X"
-            Alterar_jogada()
-        else:
-            dados[2][1] = "O"
-            Alterar_jogada()
-
-    elif jogada == 9:
-        if jogada_X:
-            dados[2][2] = "X"
-            Alterar_jogada()
-        else:
-            dados[2][2] = "O"
-            Alterar_jogada()
-
-            
-                
+    if jogada_X:
+        dados[linha][coluna] = "X"
+        Alterar_jogada()
+    else:
+        dados[linha][coluna] = "O"
+        Alterar_jogada()
     
-        
+    ganhou = False
+    
+    for linha in range(3):
+        if dados[linha][0] == dados[linha][1] == dados[linha][2]:
+            ganhou = True
+            break
+
+    for coluna in range(3):
+        if dados[0][coluna] == dados[1][coluna] == dados[2][coluna]:
+            ganhou = True
+            break
+
+    if dados[0][0] == dados[1][1] == dados[2][2] and dados[0][0] in ("X", "O"):
+        ganhou = True
+
+    elif dados[0][2] == dados[1][1] == dados[2][0] and dados[0][2] in ("X", "O"):
+        ganhou = True
+    
+    if ganhou:
+        if not jogada_X:
+            print("\n\nX GANHOU !!!\n\n\n")
+        else:
+            print("\n\nO GANHOU !!!\n\n\n")
+        break
+
     
 
