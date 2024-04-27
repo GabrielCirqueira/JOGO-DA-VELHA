75a081bb343194004145c15203d4065aaf0c6123
OLD-gabriel <gabrielcirqueira711@gmail.com>
2024-04-27 16:32:00 -0300

    TABELA CRIADA

--- jogo_velha.py
+++ jogo_velha.py
@@ -1,1 +1,18 @@
 # JOGO DA VELHA
+
+dados = [
+        ['1', '2', '3'],
+        ['4', '5', '6'],
+        ['7', '8', '9']
+    ]
+
+def tabela(dados):
+    print(f" {dados[0][0]} | {dados[0][1]} | {dados[0][2]} ")
+    print("-----------")
+    print(f" {dados[1][0]} | {dados[1][1]} | {dados[1][2]} ")
+    print("-----------")
+    print(f" {dados[2][0]} | {dados[2][1]} | {dados[2][2]} ")
+    
+    
+
+
